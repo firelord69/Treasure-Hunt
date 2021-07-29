69b74f9afc0e786cc0c23ace71d4e98bfe9f6951
firelord69 <79193827+firelord69@users.noreply.github.com>
2021-07-29 19:21:41 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+    fill("white")
+    textSize(15)
+    text("U C F T O I N N", 700,150)
+    fill("lightblue")
+    text("Hint: It Perform A Particular Task!!",700,180)
+
+    
+    fill("white")
+    textSize(15)
+    text("E D B A S T A A", 100,250)
+    fill("lightblue")
+    text("Hint: I Stores All Information Bhidu!!",100,270)
+    
+
+}
